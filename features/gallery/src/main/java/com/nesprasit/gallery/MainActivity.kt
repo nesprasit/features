d7ffb0d2c43ac4a19a1da2bdee2c3baa7faa1f59
Prasit.s <Prasit.s@clicknext.com>
2020-09-28 15:30:10 +0700

    build

---
+++ features/gallery/src/main/java/com/nesprasit/gallery/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nesprasit.gallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
